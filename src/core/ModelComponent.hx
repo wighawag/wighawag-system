adcd9339dd0fc9afd9b4c590d29755f4765ff9e0
Wighawag <wighawag@gmail.com>
2012-07-23 12:39:39 +0100

    add some TODOs

--- src/core/ModelComponent.hx
+++ src/core/ModelComponent.hx
@@ -1,3 +1,4 @@
 package core;
 interface ModelComponent implements Component{
+    //TODO Move model from SystemComponent here (need to extract teh correspondign macro code as well
 }
--- src/core/SystemComponent.hx
+++ src/core/SystemComponent.hx
@@ -3,5 +3,6 @@ package core;
 @:autoBuild(core.SystemComponentMacro.build())
 interface SystemComponent implements ModelComponent {
     public var model(default, setModel) : Model;
+// TODO add entities ?
     public function update(dt : Float) : Void;
 }
